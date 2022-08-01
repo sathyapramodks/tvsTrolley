24ba5164a3563b97ef79f3c9f36979baa6de8805
Sathya Pramod K S <67020305+sathyapramodks@users.noreply.github.com>
2022-08-01 09:38:39 +0530

    Update index.py

--- index.py
+++ index.py
@@ -180,7 +180,7 @@ check4 = stm.radio("Choose the appropriate", ('Select if you want to auto calcul
 
 if check4 == 'Select if you want to auto calculate clearance between parts':
      stm.write('Part to part clearance will be calculated automatically \n 15 "%" of the dimension ')
-elif check4 = 'Select if you want to protrude higher than the height of the trolley':
+elif check4 == 'Select if you want to protrude higher than the height of the trolley':
     stm.write('Parts can be stored to a maximum height of 15% the maximum trolley height')
 else:
     clen = stm.number_input("Enter the clearance between parts in length direction ")
